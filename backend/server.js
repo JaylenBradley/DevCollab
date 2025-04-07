a1136edf438e4448f3a1f2bc25feb9b4df5ded4d
Julian Dalban <julian.dalban@gmail.com>
2025-04-06 21:18:22 -0400

    Added the new router to server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const usersRouter = require('./routes/users');
 const projectsRouter = require('./routes/projects');
+const applicationsRouter = require('./router/applications');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,3 +12,4 @@ connectDB(app, port);
 app.use(express.json()); // Middleware to parse JSON -> code that runs when server gets a request, but before it gets passed to routes
 app.use('/users', usersRouter);
 app.use('/projects', projectsRouter);
+app.use('/applications', applicationsRouter);
